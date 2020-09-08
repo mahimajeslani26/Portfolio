bb319babb332d88bd8b8601ed6cadbcec5c00303
Mahima Jeslani <mahimajeslani@Prernas-MacBook-Pro.local>
2020-09-08 17:42:38 +0530

    Updates

---
+++ precache-manifest.9db689f17e61c0be6f01ec8b04d1e356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca8e8771ce34872cc48698c17f68361",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "9553b8766c203c502a02",
+    "url": "/Portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/Portfolio/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/Portfolio/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9553b8766c203c502a02",
+    "url": "/Portfolio/static/js/main.e524a8ed.chunk.js"
+  },
+  {
+    "revision": "b4f6ea8748b4b447c1e0",
+    "url": "/Portfolio/static/js/runtime-main.d4d59c43.js"
+  }
+]);
